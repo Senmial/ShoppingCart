c5c63a632c40bc35da005889d3d598d95dfbf0a6
Sennie <senmial@gmail.com>
2024-03-16 10:56:44 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Assigning all buttons and elements
+const incrementButton = document.querySelectorAll('.increment');
+const decrementButton = document.querySelectorAll('.decrement');
+const removeButton = document.querySelectorAll('.remove');
+const heartIcon = document.querySelectorAll('.heart');
+const totalPriceElement = document.getElementById('totalPrice');
+let totalPrice = 139800; // Initial total price before increment or decrement
+
+//Function to update the total price
